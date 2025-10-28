0b2d14795d7f6cffc9e13d8c001b5aba37491dd9
Xenia Gorbunova <GorbunovaKsenia@gmail.com>
2025-10-28 10:23:06 +0300

    chore: add styles to modal

--- app/src/features/todos/Delete/ui.tsx
+++ app/src/features/todos/Delete/ui.tsx
@@ -14,7 +14,7 @@ export const Delete: FC<DeleteProps> = ({ id, goBack }) => {
     const navigate = useNavigate();
     const deleteHandler = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        
+
         if (isPending) return
 
         await mutateAsync({id});
@@ -36,13 +36,17 @@ export const Delete: FC<DeleteProps> = ({ id, goBack }) => {
     
     return (isModalOpen 
             ? <Modal>
-                <p>Are you sure you want to delete this task?</p>
-                <button type="button" onClick={closeModalHandler} className="btn btn-primary mx-1">
-                    Cancel
-                </button>
-                <button type="button" onClick={deleteHandler} className="btn btn-danger mx-1">
-                    Delete
-                </button>
+                <div className="d-flex align-items-center flex-column gap-4">
+                    <h4>Are you sure you want to delete this task?</h4>
+                    <div className="d-flex align-items-center">
+                        <button type="button" onClick={closeModalHandler} className="btn btn-primary mx-1">
+                            Cancel
+                        </button>
+                        <button type="button" onClick={deleteHandler} className="btn btn-danger mx-1">
+                            Delete
+                        </button>
+                    </div>
+                </div>
               </Modal>
             : <button type="button" onClick={openModalHandler} className="btn btn-danger mx-1">
                 Delete
